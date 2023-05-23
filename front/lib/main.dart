5849b6cfaa6d12cb5227517dce2ca8a03e70559c
Gvozdeva Darya <gvozdevadara1@gmail.com>
2023-05-23 21:18:13 +0300

    pretty main page

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:happy_science_app/login_page.dart';
 
@@ -10,11 +12,13 @@ class SignUpApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/': (context) => const WelcomeScreen(),
+        // '/': (context) => WelcomeScreen(),
         '/signup': (context) => const SignUpScreen(),
         '/register': (context) => const RegScreen(),
         '/home': (context) => const HomeScreen(),
       },
+      home: WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -57,42 +61,42 @@ class RegScreen extends StatelessWidget {
   }
 }
 
-class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
+// class WelcomeScreen extends StatelessWidget {
+//   const WelcomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              child: Text('Log in',
-                  style: Theme.of(context).textTheme.displayMedium),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignUpScreen()),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: Text('Sign up',
-                  style: Theme.of(context).textTheme.displayMedium),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegScreen()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Row(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             ElevatedButton(
+//               child: Text('Log in',
+//                   style: Theme.of(context).textTheme.displayMedium),
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => const SignUpScreen()),
+//                 );
+//               },
+//             ),
+//             ElevatedButton(
+//               child: Text('Sign up',
+//                   style: Theme.of(context).textTheme.displayMedium),
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => const RegScreen()),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class RegForm extends StatefulWidget {
   const RegForm({super.key});
@@ -108,7 +112,7 @@ class _RegFormState extends State<RegForm> {
   final _usernameTextController = TextEditingController();
 
   void _showWelcomeScreen() {
-    Navigator.of(context).pushNamed('/home');
+    Navigator.of(context as BuildContext).pushNamed('/home');
   }
 
   @override
@@ -177,7 +181,7 @@ class _SignUpFormState extends State<SignUpForm> {
 
   // ignore: unused_element
   void _showWelcomeScreen() {
-    Navigator.of(context).pushNamed('/home');
+    Navigator.of(context as BuildContext).pushNamed('/home');
   }
 
   @override
@@ -293,10 +297,142 @@ class HomeScreen extends StatelessWidget {
     return const Scaffold(
       backgroundColor: Color.fromARGB(255, 207, 229, 195),
       body: Center(
-        child: SizedBox(
-          width: 400,
-          child: Card(
-            child: HomePage(),
+        child: SizedBox(),
+      ),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 240, 238, 238),
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width / 8),
+        children: const [
+          Menu(),
+          // MediaQuery.of(context).size.width >= 980
+          //     ? Menu()
+          //     : SizedBox(), // Responsive
+          Body()
+        ],
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  const Menu({super.key});
+
+  void _showSignIn() {
+    Navigator.of(context as BuildContext).pushNamed('/signup');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              _menuItem(title: 'Home'),
+              _menuItem(title: 'About us'),
+              _menuItem(title: 'Contact us'),
+              _menuItem(title: 'Help'),
+            ],
+          ),
+          Row(
+            children: [
+              _menuItem(title: 'Sign In', isActive: true),
+              _registerButton()
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _menuItem({String title = 'Title Menu', isActive = false}) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 75),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Column(
+          children: [
+            Text(
+              '$title',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: isActive ? Colors.deepPurple : Colors.grey,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            isActive
+                ? Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 2),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  )
+                : const SizedBox()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(255, 238, 238, 238),
+            spreadRadius: 10,
+            blurRadius: 12,
+          ),
+        ],
+      ),
+      child: TextButton(
+        style: ButtonStyle(
+          foregroundColor:
+              MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+            return states.contains(MaterialState.disabled)
+                ? null
+                : Colors.white;
+          }),
+          // backgroundColor:
+          //     MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+          //   return states.contains(MaterialState.disabled)
+          //       ? null
+          //       : const Color.fromARGB(255, 112, 181, 237);
+          // }),
+        ),
+        onPressed: () {
+          // Navigator.push(
+          //   context as BuildContext,
+          //   MaterialPageRoute(builder: (context) => const RegScreen()),
+          // );
+          _showSignIn();
+        },
+        child: const Text(
+          'Register',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black54,
           ),
         ),
       ),
@@ -304,18 +440,236 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
-  @override
-  State<HomePage> createState() => _HomeState();
-}
-
-class _HomeState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 360,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Sign In to \nMy Application',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "If you don't have an account",
+                style: TextStyle(
+                    color: Colors.black54, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  const Text(
+                    "You can",
+                    style: TextStyle(
+                        color: Colors.black54, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(width: 15),
+                  GestureDetector(
+                    onTap: () {
+                      print(MediaQuery.of(context).size.width);
+                    },
+                    child: const Text(
+                      "Register here!",
+                      style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Image.asset(
+                'images/illustration-2.png',
+                width: 300,
+              ),
+            ],
+          ),
+        ),
+
+        Image.asset(
+          'images/illustration-1.png',
+          width: 300,
+        ),
+        // MediaQuery.of(context).size.width >= 1300 //Responsive
+        //     ? Image.asset(
+        //         'images/illustration-1.png',
+        //         width: 300,
+        //       )
+        //     : SizedBox(),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height / 6),
+          child: Container(
+            width: 320,
+            child: _formLogin(),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _formLogin() {
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Enter email or Phone number',
+            filled: true,
+            fillColor: Colors.blueGrey[50],
+            labelStyle: const TextStyle(fontSize: 12),
+            contentPadding: EdgeInsets.only(left: 30),
+            enabledBorder: OutlineInputBorder(
+              borderSide:
+                  const BorderSide(color: Color.fromARGB(255, 236, 239, 241)),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide:
+                  const BorderSide(color: Color.fromARGB(255, 236, 239, 241)),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        const SizedBox(height: 30),
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Password',
+            counterText: 'Forgot password?',
+            suffixIcon: const Icon(
+              Icons.visibility_off_outlined,
+              color: Colors.grey,
+            ),
+            filled: true,
+            fillColor: Colors.blueGrey[50],
+            labelStyle: TextStyle(fontSize: 12),
+            contentPadding: EdgeInsets.only(left: 30),
+            enabledBorder: OutlineInputBorder(
+              borderSide:
+                  const BorderSide(color: Color.fromARGB(255, 236, 239, 241)),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide:
+                  const BorderSide(color: Color.fromARGB(255, 236, 239, 241)),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        const SizedBox(height: 40),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromARGB(255, 209, 196, 233),
+                spreadRadius: 10,
+                blurRadius: 20,
+              ),
+            ],
+          ),
+          child: ElevatedButton(
+            onPressed: () => print("it's pressed"),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.deepPurple,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            child: Container(
+                width: double.infinity,
+                height: 50,
+                child: Center(child: Text("Sign In"))),
+          ),
+        ),
+        const SizedBox(height: 40),
+        Row(children: [
+          Expanded(
+            child: Divider(
+              color: Colors.grey[300],
+              height: 50,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text("Or continue with"),
+          ),
+          Expanded(
+            child: Divider(
+              color: Colors.grey[400],
+              height: 50,
+            ),
+          ),
+        ]),
+        SizedBox(height: 40),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _loginWithButton(image: 'images/google.png'),
+            _loginWithButton(image: 'images/github.png', isActive: true),
+            _loginWithButton(image: 'images/facebook.png'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _loginWithButton({required String image, bool isActive = false}) {
+    return Container(
+      width: 90,
+      height: 70,
+      decoration: isActive
+          ? BoxDecoration(
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromARGB(255, 218, 218, 218),
+                  spreadRadius: 10,
+                  blurRadius: 30,
+                )
+              ],
+              borderRadius: BorderRadius.circular(15),
+            )
+          : BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              border:
+                  Border.all(color: const Color.fromARGB(255, 232, 231, 231)),
+            ),
+      child: Center(
+          child: Container(
+        decoration: isActive
+            ? BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(35),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 189, 189, 189),
+                    spreadRadius: 2,
+                    blurRadius: 15,
+                  )
+                ],
+              )
+            : const BoxDecoration(),
+        child: Image.asset(
+          '$image',
+          width: 35,
+        ),
+      )),
+    );
   }
 }
-
-
